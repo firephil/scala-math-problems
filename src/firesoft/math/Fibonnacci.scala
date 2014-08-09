5167c683fb6736865b7d5134e700e253165f908d
firephil <philip@firesoft.gr>
2014-08-09 13:11:26 +0300

    Functional style fibonacci with Stream[BigInt]

--- src/firesoft/math/Fibonnacci.scala
+++ src/firesoft/math/Fibonnacci.scala
@@ -3,7 +3,7 @@ import scala.annotation.tailrec
 
 // if n > 46 integer overflow
 
-object Fibonnacci {
+object Fibonacci {
 
 	
 	def imperative( n : Int ) : Int = {
@@ -20,6 +20,19 @@ object Fibonnacci {
 	a
 	}
 	
+	def imperativeLong( n : Int ) : Long = {
+	  var a = 0L
+	  var b = 1L
+	  var i = 0L	  
+	 
+	  while( i < n ) {
+	    val c = a + b
+	    a = b
+	    b = c
+	    i = i + 1
+	  } 
+	a
+	}
 	def imperativeBig( n : Int ) : BigInt = {
 	  var a = BigInt(0)
 	  var b = BigInt(1)
@@ -57,10 +70,20 @@ object Fibonnacci {
 	  
 	  aux(n,1,0)
 	}
-	
-	// not correct
-	def functional(n : Int) :Int = { 
-			
-	  (0 to n).reduce(_+_)
+		
+	def stream(n : Int) : BigInt = {
+	  
+	  def aux(h: BigInt, n: BigInt): Stream[BigInt] = h #:: aux(n, h + n)
+	  
+	  val res = aux(0, 1).take(n+1).takeRight(1)
+	  
+	 
+	  res.head
 	}
+	
+	def zipStream(n :Int) :BigInt = {
+	  lazy val fibs: Stream[BigInt] = BigInt(0) #::BigInt(1) #::fibs.zip(fibs.tail).map { n => n._1 + n._2 }
+	  fibs.take(n+1).takeRight(1).head
+	}
+	
 }
